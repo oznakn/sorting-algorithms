6d3345e11ae17073d053b7a12fadd911fba5274c
Vagharshak <vagharshak@ip-10-10-10-80.eu-central-1.compute.internal>
2019-10-18 15:11:39 +0200

    recursive mergesort in java

---
+++ merge-sort/merge_sort.java
@@ -0,0 +1,56 @@
+package de.edeka.lunar.dpp.marketservice;
+
+public class MergeSort {
+
+    public void mergeSort(int[] unsorted) {
+        if (unsorted == null) {
+            return;
+        }
+
+        if (unsorted.length > 1) {
+            int middle = unsorted.length / 2;
+
+            int[] leftPart = new int[middle];
+            int[] rightPart = new int[unsorted.length - middle];
+
+            for (int i = 0; i < middle; i++) {
+                leftPart[i] = unsorted[i];
+            }
+
+            for (int i = middle; i < unsorted.length; i++) {
+                rightPart[i - middle] = unsorted[i];
+            }
+
+            mergeSort(leftPart);
+            mergeSort(rightPart);
+
+            int comparePosLeft = 0;
+            int comparePosRight = 0;
+            int replacementPos = 0;
+
+            while (comparePosLeft < leftPart.length && comparePosRight < rightPart.length) {
+                if (leftPart[comparePosLeft] <= rightPart[comparePosRight]) {
+                    unsorted[replacementPos] = leftPart[comparePosLeft];
+                    comparePosLeft++;
+                } else {
+                    unsorted[replacementPos] = rightPart[comparePosRight];
+                    comparePosRight++;
+                }
+                replacementPos++;
+            }
+
+            collectRemainingValues(unsorted, leftPart, comparePosLeft, replacementPos);
+            collectRemainingValues(unsorted, rightPart, comparePosRight, replacementPos);
+        }
+    }
+
+    private void collectRemainingValues(int[] unsorted, int[] part, int comparePos, int replacementPos) {
+        if (comparePos < part.length) {
+            for (int i = comparePos; i < part.length; i++) {
+                unsorted[replacementPos] = part[i];
+                replacementPos++;
+            }
+        }
+    }
+
+}
